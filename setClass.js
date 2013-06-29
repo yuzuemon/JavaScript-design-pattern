6c4097b5d1e7629db523b18e825b5eea35f8a83b
yuzuemon <killerqueen64@gmail.com>
2013-06-30 00:16:37 +0900

    1章まで

--- setClass.js
+++ setClass.js
@@ -7,7 +7,12 @@ function Car(model) {
   this.color = 'silver';
   this.year = '2012';
 
-  this.getNifo = function(){
+  this.getInfo = function(){
     return this.model + ' ' + this.year;
   };
 }
+
+
+var myCar = new Car('ford');
+myCar.year = '2000';
+console.log(myCar.getInfo());
